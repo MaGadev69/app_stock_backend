5a450d2661f7daa90b53e830fd7d43e5f7a3b1c0
MaGadev69 <maxigasquez@gmail.com>
2025-04-30 02:58:03 -0300

    CORS

--- app_stock_backend/app/api/api_main.py
+++ app_stock_backend/app/api/api_main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import productos  # cuando agreguemos más routers, los importás acá
 # CONSTRUCTOR PRINCIPAL
 app = FastAPI()
+
+# Configuración de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite todos los orígenes, puedes especificar los dominios específicos si lo deseas
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos los métodos HTTP, como GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"],  # Permite todos los encabezados
+)
 
 app.include_router(productos.router, prefix="/productos", tags=["productos"])
 # quiero que todo lo que esté en el router productos esté disponible bajo la URL /productos
