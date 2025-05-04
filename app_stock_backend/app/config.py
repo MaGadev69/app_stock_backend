cfd1f2b0aa4ae1e6f2d43991e0f5059db9117740
MaGadev69 <maxigasquez@gmail.com>
2025-05-04 20:30:40 -0300

    primera prueba produccion

--- app_stock_backend/app/config.py
+++ app_stock_backend/app/config.py
@@ -4,6 +4,11 @@ import os
 load_dotenv()
 
 class Settings:
+    # Si esta linea esta habilitada, la variable DATABASE_URL se obtiene del entorno (NEON)
+    # Si no, se obtiene de las variables de entorno DB_HOST, DB_PORT, DB_NAME, DB_USER y DB_PASSWORD (DELL)
+    DATABASE_URL_ENV: str = os.getenv("DATABASE_URL")
+
+    # Variables de entorno
     DB_HOST: str = os.getenv("DB_HOST", "localhost")
     DB_PORT: str = os.getenv("DB_PORT", "5432")
     DB_NAME: str = os.getenv("DB_NAME", "postgres")
@@ -12,6 +17,8 @@ class Settings:
 
     @property
     def DATABASE_URL(self):
+        if self.DATABASE_URL_ENV:
+            return self.DATABASE_URL_ENV
         return f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
 # global
